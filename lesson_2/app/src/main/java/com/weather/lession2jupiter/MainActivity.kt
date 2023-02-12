fe6acc9661163503c3d069ccccce7ef35a4373e4
sergejmehonosin <sergey01051997@mail.ru>
2023-02-12 23:36:01 +0300

    Настройка вариантов сборки - SourceSets

--- lesson_2/app/src/main/java/com/weather/lession2jupiter/MainActivity.kt
+++ lesson_2/app/src/main/java/com/weather/lession2jupiter/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.weather.lession2jupiter
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // присвоение индификатора к textView
+        val textView = findViewById<TextView>(R.id.textView)
+
+        // устанавливаем текст
+        textView.text = """
+            Build type = ${BuildConfig.BUILD_TYPE}
+            Flavor = ${BuildConfig.FLAVOR}
+            VersionCode = ${BuildConfig.VERSION_CODE}
+            VersionName = ${BuildConfig.VERSION_NAME}
+        """
     }
 }
