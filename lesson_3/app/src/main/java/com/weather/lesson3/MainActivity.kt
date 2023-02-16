32d63d7601c63013957aa0b416198b35d17bc953
sergejmehonosin <sergey01051997@mail.ru>
2023-02-16 09:27:12 +0300

    Ресурсы приложения

--- lesson_3/app/src/main/java/com/weather/lesson3/MainActivity.kt
+++ lesson_3/app/src/main/java/com/weather/lesson3/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.weather.lesson3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
+
+        val textView = findViewById<TextView>(R.id.second_textView)
+        val count = 5
+        val pluralString = resources.getQuantityString(R.plurals.main_quantity_string, count, count)
+        textView.text = pluralString
     }
 }
