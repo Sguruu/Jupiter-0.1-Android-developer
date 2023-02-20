58b9f63132c4aa4a955ba4578577106d95bfb8a1
sergejmehonosin <sergey01051997@mail.ru>
2023-02-20 22:41:09 +0300

    TextView

--- lesson_4/app/src/main/java/com/weather/myapplication/MainActivity.kt
+++ lesson_4/app/src/main/java/com/weather/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.weather.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+// AppCompatActivity() класс AndroidSDK() который позволяет нам взаимойдествовать с Android
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
+    // переопределение метода onCreate который запускается когда создается активность
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Класс R автоматически создается комбилятором, через него получаем доступ ко всем ресурсам
+        // Устанавливаем для класса MainActivity контент activity_main
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+
+        // устанавливаем текст
+        textView.text = "Привет мир"
+        // установление текст из ресурса
+        textView.setText(R.string.app_name)
+
+        // получение строки из рессурсов
+        resources.getString(R.string.app_name)
     }
 }
