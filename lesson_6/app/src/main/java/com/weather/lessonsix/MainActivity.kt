a0d4a301e66b68c876014a4fcc966a1745c149f2
sergejmehonosin <sergey01051997@mail.ru>
2023-02-27 22:40:12 +0300

    Связывание view findViewById

--- lesson_6/app/src/main/java/com/weather/lessonsix/MainActivity.kt
+++ lesson_6/app/src/main/java/com/weather/lessonsix/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.weather.lessonsix
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+
+        button.setOnClickListener {
+            Toast.makeText(this, resources.getString(R.string.text_example), Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun initView() {
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
     }
 }
+
+
