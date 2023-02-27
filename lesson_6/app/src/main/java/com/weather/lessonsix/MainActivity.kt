1a104772ba9362202692e32a3a25d5a68ec17e18
sergejmehonosin <sergey01051997@mail.ru>
2023-02-27 22:54:33 +0300

    Добавление viewBinding

--- lesson_6/app/src/main/java/com/weather/lessonsix/MainActivity.kt
+++ lesson_6/app/src/main/java/com/weather/lessonsix/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.weather.lessonsix
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.weather.lessonsix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textView: TextView
-    lateinit var button: Button
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initView()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             Toast.makeText(this, resources.getString(R.string.text_example), Toast.LENGTH_SHORT)
                 .show()
         }
     }
-
-    private fun initView() {
-        textView = findViewById(R.id.textView)
-        button = findViewById(R.id.button)
-    }
 }
-
-
