d0c7e4eb7e93f7bff8b0225705242dcdd93dbe6f
sergejmehonosin <sergey01051997@mail.ru>
2023-03-08 13:53:32 +0300

    Демонстрация проблемы андрода при перевороте экрана

--- lesson_9/app/src/main/java/com/weather/lesson9/MainActivity.kt
+++ lesson_9/app/src/main/java/com/weather/lesson9/MainActivity.kt
@@ -8,9 +8,25 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private var state = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        renderTextView()
+        binding.buttonIncrement.setOnClickListener {
+            state++
+            renderTextView()
+        }
+
+        binding.buttonDecrement.setOnClickListener {
+            state--
+            renderTextView()
+        }
+    }
+
+    private fun renderTextView() {
+        binding.textView.text = state.toString()
     }
 }
