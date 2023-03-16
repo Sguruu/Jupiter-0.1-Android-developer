a4f65d9dfe5e5cbcc2a357e92f40a533c446c247
sergejmehonosin <sergey01051997@mail.ru>
2023-03-16 08:47:11 +0300

    Создание и добавление фрагмента

---
+++ lesson_11/app/src/main/java/com/weather/lesson11/InfoFragment.kt
@@ -0,0 +1,28 @@
+package com.weather.lesson11
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.weather.lesson11.databinding.FragmentInfoBinding
+
+class InfoFragment : Fragment() {
+    private var _binding: FragmentInfoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInfoBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
