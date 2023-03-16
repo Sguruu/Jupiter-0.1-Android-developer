23b0702a4212661c9d5d31f18daca5d856c490d1
sergejmehonosin <sergey01051997@mail.ru>
2023-03-16 17:59:35 +0300

    Добавление динамического фрагмента

--- lesson_11/app/src/main/java/com/weather/lesson11/MainActivity.kt
+++ lesson_11/app/src/main/java/com/weather/lesson11/MainActivity.kt
@@ -12,5 +12,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            showInfoFragment()
+        }
+    }
+
+    private fun showInfoFragment() {
+        // спомощью транзакции мы можем добавить, удалить, заменить
+        // val transaction = supportFragmentManager.beginTransaction()
+        // val infoFragment = InfoFragment()
+        /*
+        Первый аргумент id нашего контейнера,
+        Второй аргумент, класс нашего фрагмента
+         */
+        //  transaction.add(binding.fragment1.id, infoFragment)
+        // для сохранения нашей транзакции и показа его, но она не гарантирует что наш фрагмент
+        // появится сразу
+        //  transaction.commit()
+        // Сразу отобразит наш фрагмент, но не добавится в бэкстэк
+        // transaction.commitNow()
+
+        /* другой вид тогоже самого кода */
+        supportFragmentManager.beginTransaction()
+            .add(binding.fragment1.id, InfoFragment())
+            .commit()
     }
 }
