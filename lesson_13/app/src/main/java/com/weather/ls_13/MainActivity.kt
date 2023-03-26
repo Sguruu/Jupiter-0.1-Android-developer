b6ed702377cb8980173d5888f1e3789350e46872
sergejmehonosin <sergey01051997@mail.ru>
2023-03-26 18:56:58 +0300

    Дополнительные возможности ViewPager : Анимация пролистывания

--- lesson_13/app/src/main/java/com/weather/ls_13/MainActivity.kt
+++ lesson_13/app/src/main/java/com/weather/ls_13/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.weather.ls_13
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.weather.ls_13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -47,5 +50,70 @@ class MainActivity : AppCompatActivity() {
         binding.viewPager.adapter = adapter
         // позволяет настраивать предзагрузку фрагментов
         binding.viewPager.offscreenPageLimit = 3
+
+        /*
+        setCurrentItem(item, smoothScroll) — метод для программного перелистывания страниц:
+	    item — индекс страницы, которую необходимо отобразить;
+	    smoothScroll — флаг, показывающий, нужно ли перелистнуть
+	    страницу плавно с анимацией или без анимации;
+         */
+        binding.viewPager.setCurrentItem(2, false)
+        // currentItem — свойство для получения текущей страницы;
+        binding.viewPager.currentItem
+
+        // orientation — свойство для настройки направления скролла (по умолчанию — горизонтальное)
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+
+        /*
+        registerOnPageChangeCallback — колбэк для отслеживания того, что пользователь перелистнул
+        страницу у ViewPager.
+         */
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            /**
+             * @param position позиция текущего перелистывания экрана
+             */
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                showToast("текущая позиция : $position")
+            }
+        })
+
+        /*
+        Здесь:
+        pageTransformer — параметр, который показывает, на какой стадии скролла находится страница;
+        transformPage(@NotNull View page, float position) — метод, в котором
+        происходит основная логика; принимает на вход страницу и позицию её скролла:
+        position 0 — страница полностью видна пользователю;
+        position −1 — соседняя страница слева от видимой пользователю страницы
+        (если мы рассматриваем горизонтальный скролл), пользователь не видит эту страницу;
+        position 1 — соседняя страница справа от видимой пользователю страницы.
+         */
+
+        binding.viewPager.setPageTransformer(object : ViewPager2.PageTransformer {
+            /**
+             * Напишим логику, что если наши фрагменты невидимы пользователю они буду исчезать
+             */
+            override fun transformPage(page: View, position: Float) {
+                when {
+                    position < -1 || position > 1 -> {
+                        // alfa значение отвечающая за видимость view где 0 - полность не видим
+                        // 1 - видим
+                        page.alpha = 0f
+                    }
+
+                    position <= 0 -> {
+                        page.alpha = 1 + position
+                    }
+
+                    position <= 1 -> {
+                        page.alpha = 1 - position
+                    }
+                }
+            }
+        })
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
