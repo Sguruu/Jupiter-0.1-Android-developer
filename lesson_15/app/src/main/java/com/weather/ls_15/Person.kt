43329daa49be6e549d36e19e09feef9df9facf22
sergejmehonosin <sergey01051997@mail.ru>
2023-04-09 16:05:53 +0300

    Добавление функционала добавления/удаления списка

---
+++ lesson_15/app/src/main/java/com/weather/ls_15/Person.kt
@@ -0,0 +1,24 @@
+package com.weather.ls_15
+
+sealed class Person {
+
+    data class User(
+        // имя пользователя
+        val name: String,
+        // ссылка на автар из интернета
+        val avatarLink: String,
+        // возраст
+        val age: Int
+    ) : Person()
+
+    data class Developer(
+        // имя пользователя
+        val name: String,
+        // ссылка на автар из интернета
+        val avatarLink: String,
+        // возраст
+        val age: Int,
+        // языка программирования
+        val programmingLanguage: String
+    ) : Person()
+}
