29e9ce6b527c7841e2d907da0988597daf55e01a
sergejmehonosin <sergey01051997@mail.ru>
2023-05-03 22:41:54 +0300

    Подключение библиотек

--- lesson_19_1/app/src/main/java/com/weather/ls_19_1/MainActivity.kt
+++ lesson_19_1/app/src/main/java/com/weather/ls_19_1/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.weather.ls_19_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.weather.ls_19_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
