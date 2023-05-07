8b938e8488d76b1d4b8149fc9fe83ef489b37912
sergejmehonosin <sergey01051997@mail.ru>
2023-05-08 01:31:10 +0300

    Выносим работу с файлами в отдельный поток

--- lesson_20/app/src/main/java/com/weather/myapplication/MainActivity.kt
+++ lesson_20/app/src/main/java/com/weather/myapplication/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.weather.myapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
@@ -15,55 +18,62 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.playSoundButton.setOnClickListener {
-            MediaPlayer.create(this, R.raw.sound)
-                // запускаем проигрыватель
-                .start()
-        }
-
-        // Открытие потока данных для чтения необработанного ресурса
-        resources.openRawResource(R.raw.sound).use {
-            // блок кода
-        }
-
-        // свойство assets вернут assetManager
-        val assetManager = resources.assets
-        // возвращает файлы которые находятся в папке, в параметрах принимает путь до папки
-        // если передаем "" значит мы хотим просмотреть все файлы в папке asset
-        // может вернут null если совершим ошибку в пути к файлу
-        val fileListString = assetManager.list("folder1")
-            // если вернет null вернет пустой список
-            .orEmpty()
-            // объединить строки в одну строку
-            .joinToString()
-
-        Log.d("MyTest", "fileListString fileString $fileListString")
-
-        // обертка, аналог try catch
-        runCatching {
-            // получение контента функции, в параметрах нужно указать путь и имя файла
-            val text = assetManager.open("folder1/text.txt")
-                // Создает буферизованный модуль чтения для этого входного потока
-                .bufferedReader()
-                // Выполняет заданную функцию блока на этом ресурсе, а затем корректно закрывает его,
-                // независимо от того, выдано исключение или нет.
-                .use {
-                    // считать все данные из файла в одну строку
-                    it.readText()
-                }
-            Log.d("MyTest", "text open $text")
-        }
-
-        // сохраним файл в папку кеша
-        val cacheDir = this.cacheDir
-        // вывод в Log абсолютного пути папки кеша
-        Log.d("MyTest", "cacheDir path = ${cacheDir.absolutePath}")
-
-        // запись файла в папку с кешом
-        val cacheFile = File(cacheDir, "test_cache,text")
-        try {
-            cacheFile.outputStream().buffered().use {
-                it.write("Content in cache file".toByteArray())
+            CoroutineScope(Dispatchers.IO).launch {
+                MediaPlayer.create(this@MainActivity, R.raw.sound)
+                    // запускаем проигрыватель
+                    .start()
             }
-        } catch (t: Throwable){}
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Открытие потока данных для чтения необработанного ресурса
+            resources.openRawResource(R.raw.sound).use {
+                // блок кода
+            }
+
+            // свойство assets вернут assetManager
+            val assetManager = resources.assets
+            // возвращает файлы которые находятся в папке, в параметрах принимает путь до папки
+            // если передаем "" значит мы хотим просмотреть все файлы в папке asset
+            // может вернут null если совершим ошибку в пути к файлу
+            val fileListString = assetManager.list("folder1")
+                // если вернет null вернет пустой список
+                .orEmpty()
+                // объединить строки в одну строку
+                .joinToString()
+
+            Log.d("MyTest", "fileListString fileString $fileListString")
+
+            // обертка, аналог try catch
+            runCatching {
+                // получение контента функции, в параметрах нужно указать путь и имя файла
+                val text = assetManager.open("folder1/text.txt")
+                    // Создает буферизованный модуль чтения для этого входного потока
+                    .bufferedReader()
+                    // Выполняет заданную функцию блока на этом ресурсе, а затем корректно закрывает его,
+                    // независимо от того, выдано исключение или нет.
+                    .use {
+                        // считать все данные из файла в одну строку
+                        it.readText()
+                    }
+                Log.d("MyTest", "text open $text")
+            }
+        }
+
+        // lifecycleScope
+        CoroutineScope(Dispatchers.IO).launch {
+            // сохраним файл в папку кеша
+            val cacheDir = this@MainActivity.cacheDir
+            // вывод в Log абсолютного пути папки кеша
+            Log.d("MyTest", "cacheDir path = ${cacheDir.absolutePath}")
+
+            // запись файла в папку с кешом
+            val cacheFile = File(cacheDir, "test_cache,text")
+            runCatching {
+                cacheFile.outputStream().buffered().use {
+                    it.write("Content in cache file".toByteArray())
+                }
+            }
+        }
     }
 }
