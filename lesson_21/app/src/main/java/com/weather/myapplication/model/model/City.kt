2c4517b4619c694035d6e2ecacc45f3cc1910d63
sergejmehonosin <sergey01051997@mail.ru>
2023-05-09 23:16:56 +0300

    Init commit lesson 21

---
+++ lesson_21/app/src/main/java/com/weather/myapplication/model/model/City.kt
@@ -0,0 +1,8 @@
+package com.weather.myapplication.model.model
+
+data class City(
+    val name: String,
+    val imageLink: String,
+    val lat: String,
+    val lon: String
+)
---
+++ lesson_21/app/src/main/java/com/weather/myapplication/viewmodel/InfoViewModel.kt
@@ -0,0 +1,51 @@
+package com.weather.myapplication.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.weather.myapplication.base.Result
+import com.weather.myapplication.model.model.ResponseWeather
+import com.weather.myapplication.model.model.Weather
+import com.weather.myapplication.model.repository.WeatherRepository
+import kotlinx.coroutines.*
+
+class InfoViewModel : ViewModel() {
+    private val _weatherLiveData = MutableLiveData<Weather>()
+    val responseWeatherLiveData
+        get() = _weatherLiveData
+
+    private val repository = WeatherRepository()
+
+    private var currentCall: retrofit2.Call<ResponseWeather>? = null
+
+    fun requestWeather(lat: String, lon: String) {
+        viewModelScope.launch {
+            repository.requestWeather(lat, lon).apply {
+                when (this) {
+                    is Result.Success<Weather> -> {
+                        updateWeatherLiveData(this.data)
+                    }
+                    is Result.Error -> {
+                        // тут обрабатываем ошибку
+                        Log.d("MyTest", "Обработка ошибки∆")
+                    }
+                }
+            }
+        }
+    }
+
+    fun convertWeatherToJson(value: Weather): String {
+        return repository.convertWeatherToJson(value)
+    }
+
+    private fun updateWeatherLiveData(value: Weather) {
+        _weatherLiveData.postValue(value)
+        currentCall = null
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        currentCall?.cancel()
+    }
+}
