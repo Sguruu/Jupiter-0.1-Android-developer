f2d70a30b64d6f40af415f9fea3b594f39eea00d
sergejmehonosin <sergey01051997@mail.ru>
2023-05-10 22:26:05 +0300

    Создание DAO

---
+++ lesson_21/app/src/main/java/com/weather/myapplication/base/db/dao/CityDao.kt
@@ -0,0 +1,16 @@
+package com.weather.myapplication.base.db.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.weather.myapplication.base.db.models.CityContract
+import com.weather.myapplication.base.db.models.CityEntity
+
+// аннотация позволяет Room понять что нужно генерировать для имплементации
+@Dao
+interface CityDao {
+    /**
+     * Получение всех городов
+     */
+    @Query("SELECT * FROM ${CityContract.TABLE_NAME}")
+    fun getAllCity(): List<CityEntity>
+}
